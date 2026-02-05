397df2bc274a4fc1f0606f2ab8d730273938b2d0
kokoro-shoji <cocoro_0629_shoji@docomo.ne.jp>
2026-02-06 01:15:44 +0900

    github pages 404 fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+const p = new URLSearchParams(location.search).get("p");
+if (p) history.replaceState(null, "", p);
 
 createRoot(document.getElementById("root")!).render(<App />);
