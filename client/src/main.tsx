dc35db094e01e6f3907b66cff227ed0f12e36c02
kokoro-shoji <cocoro_0629_shoji@docomo.ne.jp>
2026-02-06 01:21:04 +0900

    redirect receiver added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// GitHub Pages redirect support
 const p = new URLSearchParams(location.search).get("p");
-if (p) history.replaceState(null, "", p);
+if (p) {
+  history.replaceState(null, "", p);
+}
+
 
 createRoot(document.getElementById("root")!).render(<App />);
